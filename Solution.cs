12a96afc64ae2f2086dbe424ebc94e042194c100
Tom Baak 1058829 <1058829@hr.nl>
2023-11-03 12:13:00 +0100

    q3

--- Solution.cs
+++ Solution.cs
@@ -1,4 +1,6 @@
 
+using System.Data;
+
 class Solution
 {
     
@@ -14,12 +16,45 @@ class Solution
 
     public static void Q2(ExamContext db, int ProductID)
     {
-       
+       var productInfo = from prod in db.Products
+                        join comp in db.Companies on prod.CompanyID equals comp.ID
+                        where prod.ID == ProductID
+                         select new {
+                            companyName = comp.Name,
+                            name = prod.Name,
+                            price = prod.Price
+                         };  
+        
+        var product = productInfo.FirstOrDefault();
+
+        Console.WriteLine($"Company: {product.companyName}, Prodcut: {product.name}, Price: {product.price}");
     }
 
     public static void Q3(ExamContext db)
     {
         
+        var countries = db.Companies
+                        .GroupBy(c => c.Country == null ? "NULL" : c.Country.ToUpper())
+                        .Select(group => new {
+                            Country = group.Key,
+                            Total = group.Count(),
+                            CompanyInfo = group.Select(group => new {
+                                group.ID,
+                                group.Name
+                            })
+                        }).ToList();
+
+        foreach (var item in countries)
+        {
+            Console.WriteLine($"{item.Country}, {item.Total}");
+
+            foreach (var info in item.CompanyInfo)
+            {
+                Console.WriteLine($"{info.ID}, {info.Name}");
+            }
+
+        }
+
     }
 
     public static void Q4(ExamContext db, int OrderID)
